1170eaf877450dd9527284af1d88d59e898f9028
Alexander Kovrovsky <a.s.kovrovskiy@gmail.com>
2022-11-14 15:47:17 +0300

    Update 2022/11/14

---
+++ PassChange.py
@@ -0,0 +1,45 @@
+import time
+from netmiko import ConnectHandler
+import sys
+
+print("########################################")
+print("####  User password change script.  ####")
+print("########################################")
+print(" ")
+
+ipAddr = input("Enter ip address: ")
+loginMik = input("Enter login mikrotik: ")
+passMik = input("Enter password mikrotik: ")
+
+mikrotik_router_1 = {
+'device_type': 'mikrotik_routeros',
+'host': ipAddr,
+'port': '22',
+'username': loginMik,
+'password': passMik
+}
+sshCli = ConnectHandler(**mikrotik_router_1)
+
+listUsers = sshCli.send_command("/user print detail")
+strStart = "name="
+strEnd = "group"
+start = -1
+count = 0
+listStart = 0
+
+resStart = [i for i in range(len(listUsers)) if listUsers.startswith(strStart, i)]
+resEnd = [i for i in range(len(listUsers)) if listUsers.startswith(strEnd, i)]
+resFull = resStart + resEnd
+
+while count < 2:
+    start = listUsers.find(strStart, start+1)
+    if start == -1:
+        break
+    count += 1
+print("Number of users:", count)
+
+while listStart < count:
+    print(listStart+1,"-", listUsers[resFull[listStart]+5:resFull[listStart+2]])
+    listStart += 1
+
+sshCli.disconnect()
