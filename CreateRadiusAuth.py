ebf9182d713e99b5c7b59e2adcc905aa88d7a66b
Alexander Kovrovsky <a.s.kovrovskiy@gmail.com>
2022-11-14 16:33:11 +0300

    Update 2022/11/14

--- CreateRadiusAuth.py
+++ CreateRadiusAuth.py
@@ -2,9 +2,10 @@ import time
 from netmiko import ConnectHandler
 import sys
 
-ipAddr = input("Enter ip address: ")
-loginMik = input("Enter login mikrotik: ")
-passMik = input("Enter password mikrotik: ")
+# Entering data to connect.
+ipAddr = input("Enter ip address Mikrotik: ")
+loginMik = input("Enter login Mikrotik: ")
+passMik = input("Enter password Mikrotik: ")
 
 mikrotik_router_1 = {
 'device_type': 'mikrotik_routeros',
@@ -14,10 +15,15 @@ mikrotik_router_1 = {
 'password': passMik
 }
 
+# Opening ssh connection.
 sshCli = ConnectHandler(**mikrotik_router_1)
 
+addressRadiusServer = input("Enter ip address Radius Server: ")
+secretRadius = input("Enter secret Radius Server: ")
+
+# Enable AAA authorization and reset server radius settings.
 enableUseRadius = "/user aaa set use-radius=yes"
-addRadiusAuth = "/radius add service=login address=192.168.254.7 secret=komplexhost"
+addRadiusAuth = f"/radius add service=login address={addressRadiusServer} secret={secretRadius}"
 showComOne = "/radius print detail"
 showComTwo = "/user aaa print" 
 comdOne = sshCli.send_command(enableUseRadius)
@@ -26,8 +32,10 @@ time.sleep(1)
 print(sshCli.send_command(showComOne))
 time.sleep(1)
 print(sshCli.send_command(showComTwo))
+# Closing the ssh connection.
 sshCli.disconnect()
 print(" ")
 print("Successfully!")
+# Wait 10 seconds and close the console.
 time.sleep(10)
 sys.exit
